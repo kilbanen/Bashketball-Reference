868696c9f4ecd74e18fb1df8e90b7e690569d6db
Kilbane <kilbanen@tcd.ie>
2021-01-28 09:15:46 +0000

    The script now takes two arguments and works for all players

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -s https://www.basketball-reference.com/players/a/ | awk -v player="$1" -F'\"' '$0~player{printf "https://basketball-reference.com%s\n",$10}'
+first_name=$1
+last_name=$2
+full_name="$first_name $last_name"
+second_initial="${last_name:0:1}"
+url="https://www.basketball-reference.com/players/${second_initial,}/"
+curl -s $url | awk -v player="$full_name" -F'\"' '$0~player{printf "https://basketball-reference.com%s\n",$10}'
