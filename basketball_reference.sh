b66955c92d5a5f46d6cfe85b05c4e83a08beef06
Kilbane <kilbanen@tcd.ie>
2021-01-28 09:37:39 +0000

    Removed extra lines that appeared for popular players

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -4,4 +4,4 @@ last_name=$2
 full_name="$first_name $last_name"
 second_initial="${last_name:0:1}"
 url="https://www.basketball-reference.com/players/${second_initial,}/"
-curl -s $url | awk -v player="$full_name" -F'\"' '$0~player{printf "https://basketball-reference.com%s\n",$10}'
+curl -s $url | awk -v player="$full_name" -F'\"' '$11~player{printf "https://basketball-reference.com%s\n",$10}'
