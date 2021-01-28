d5df1acaaaa707ce82dbfe4804899cb896b11177
Kilbane <kilbanen@tcd.ie>
2021-01-28 15:22:09 +0000

    Added error message

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+[ $# != 2 ] && echo "Usage: ./basketball_reference.sh firstname lastname" && exit 1
 first_name=$1
 last_name=$2
 full_name="$first_name $last_name"
 second_initial="${last_name:0:1}"
 url="https://www.basketball-reference.com/players/${second_initial,}/"
-curl -s $url | awk -v player="$full_name" -F'\"' '$11~player{ gsub ("*", ""); gsub ("</strong>",""); gsub ("</a></th><td class=",""); gsub (">",""); printf "%s: https://basketball-reference.com%s\n",$11,$10}'
+curl -s $url | awk -v player="$full_name" -F'\"' '$11~player{
+  gsub ("*", "");
+  gsub ("</strong>","");
+  gsub ("</a></th><td class=","");
+  gsub (">","");
+  printf "%s: https://basketball-reference.com%s\n",$11,$10
+}'
