c421d214ba3120554f7c204315ef222857ee8387
Kilbane <kilbanen@tcd.ie>
2021-01-29 20:40:29 +0000

    Forgot to add exit codes

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -12,12 +12,14 @@
     gsub ("</a></th><td class=","");
     gsub (">","");
     printf "%s: https://basketball-reference.com%s\n",$11,$10
-  }' ||
+  }' &&
+  exit 0 ||
 [ $1 == "-p" ] && echo "Usage: -p firstname lastname" && exit 0 ||
 [ $1 == "-t" ] && [ $# == 2 ] &&
   team_name=$2 &&
   url="https://www.basketball-reference.com/teams/" &&
   curl -s $url | awk -v team="full_table.*$team_name" -F'\"' '$0~team{
     printf "https://basketball-reference.com%s\n",$10
-  }' ||
+  }' &&
+  exit 0 ||
 [ $1 == "-t" ] && echo "Usage: -t team" && exit 0
