db5fc1fefffd7cef7dbee2efb7f4a34edfffb05d
Kilbane <kilbanen@tcd.ie>
2021-01-29 20:25:25 +0000

    Switched out if statements for logical operators

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
-if [ $1 == "-p" ]
-then
-  if [ $# != 3 ]
-  then
-    echo "Usage: ./basketball_reference.sh -p firstname lastname"
-    exit 0
-  fi
-  first_name=$2
-  last_name=$3
-  full_name="$first_name $last_name"
-  second_initial="${last_name:0:1}"
-  url="https://www.basketball-reference.com/players/${second_initial,}/"
+[ -z "$1" ] && echo "Not enough arguments" && exit 0 ||
+[ $1 == "-p" ] && [ $# == 3 ] &&
+  first_name=$2 &&
+  last_name=$3 &&
+  full_name="$first_name $last_name" &&
+  second_initial="${last_name:0:1}" &&
+  url="https://www.basketball-reference.com/players/${second_initial,}/" &&
   curl -s $url | awk -v player="$full_name" -F'\"' '$11~player{
     gsub ("*", "");
     gsub ("</strong>","");
     gsub ("</a></th><td class=","");
     gsub (">","");
     printf "%s: https://basketball-reference.com%s\n",$11,$10
-  }'
-elif [ $1 == "-t" ] 
-then
-  if [ $# != 2 ]
-  then
-    echo "Usage: ./basketball_reference.sh -t team"
-    exit 0
-  fi
-  team_name=$2
-  url="https://www.basketball-reference.com/teams/"
+  }' ||
+[ $1 == "-p" ] && echo "Usage: -p firstname lastname" && exit 0 ||
+[ $1 == "-t" ] && [ $# == 2 ] &&
+  team_name=$2 &&
+  url="https://www.basketball-reference.com/teams/" &&
   curl -s $url | awk -v team="full_table.*$team_name" -F'\"' '$0~team{
     printf "https://basketball-reference.com%s\n",$10
-  }'
-else
-  echo "Fail"
-fi
+  }' ||
+[ $1 == "-t" ] && echo "Usage: -t team" && exit 0
