be6509d4c0ce579794fae5fab5ae641d89e73a2c
Kilbane <kilbanen@tcd.ie>
2021-01-30 17:40:15 +0000

    Deobfuscated code - not using if statements is bad advice

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
-[ -z "$1" ] && echo "Not enough arguments" && exit 0 ||
-[ $1 == "-p" ] && [ $# == 3 ] &&
-  first_name=$2 &&
-  last_name=$3 &&
-  full_name="$first_name $last_name" &&
-  second_initial="${last_name:0:1}" &&
-  url="https://www.basketball-reference.com/players/${second_initial,}/" &&
-  curl -s $url | awk -v player="$full_name" -F'\"' '$11~player{
-    gsub ("*", "");
-    gsub ("</strong>","");
-    gsub ("</a></th><td class=","");
-    gsub (">","");
-    printf "%s: https://basketball-reference.com%s\n",$11,$10
-  }' &&
-  exit 0 ||
-[ $1 == "-p" ] && echo "Usage: -p firstname lastname" && exit 0 ||
-[ $1 == "-t" ] && [ $# == 2 ] &&
-  team_name=$2 &&
-  url="https://www.basketball-reference.com/teams/" &&
-  curl -s $url | awk -v team="full_table.*$team_name" -F'\"' '$0~team{
-    gsub ("*", "");
-    gsub ("</strong>","");
-    gsub ("</a></th><td class=","");
-    gsub (">","");
-    printf "%s: https://basketball-reference.com%s\n",$11,$10
-  }' &&
-  exit 0 ||
-[ $1 == "-t" ] && echo "Usage: -t team" && exit 0
+if [ -z "$1" ]
+then 
+  echo "Not enough arguments"
+  exit 0
+elif [ $1 == "-p" ] 
+then
+  if [ $# == 3 ]
+  then
+    first_name=$2
+    last_name=$3
+    full_name="$first_name $last_name"
+    second_initial="${last_name:0:1}"
+    url="https://www.basketball-reference.com/players/${second_initial,}/"
+    search_pattern="tr.*$full_name"
+  else
+    echo "Usage: -p firstname lastname"
+    exit 0
+  fi
+elif [ $1 == "-t" ]
+then
+  if [ $# == 2 ]
+  then
+    team_name=$2
+    url="https://www.basketball-reference.com/teams/"
+    search_pattern="full_table.*$team_name"
+  else
+    echo "Usage: -t team"
+    exit 0
+  fi
+fi
+curl -s $url | awk -v pattern="$search_pattern" -F'\"' '$0~pattern{
+  gsub ("*", "");
+  gsub ("</strong>","");
+  gsub ("</a></th><td class=","");
+  gsub (">","");
+  printf "%s: https://basketball-reference.com%s\n",$11,$10
+}'
