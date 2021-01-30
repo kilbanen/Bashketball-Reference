c253b445beaca6326ffaff4a009fa60e1a36a527
Kilbane <kilbanen@tcd.ie>
2021-01-30 16:26:09 +0000

    Displpay team name and url

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -19,7 +19,11 @@
   team_name=$2 &&
   url="https://www.basketball-reference.com/teams/" &&
   curl -s $url | awk -v team="full_table.*$team_name" -F'\"' '$0~team{
-    printf "https://basketball-reference.com%s\n",$10
+    gsub ("*", "");
+    gsub ("</strong>","");
+    gsub ("</a></th><td class=","");
+    gsub (">","");
+    printf "%s: https://basketball-reference.com%s\n",$11,$10
   }' &&
   exit 0 ||
 [ $1 == "-t" ] && echo "Usage: -t team" && exit 0
