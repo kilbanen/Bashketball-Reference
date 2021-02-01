5f39f5b436c3677a37e709c065f3c4147ebee0f2
Kilbane <kilbanen@tcd.ie>
2021-02-01 17:09:25 +0000

    Added year to team query

--- basketball_reference.sh
+++ basketball_reference.sh
@@ -9,29 +9,32 @@ then
     second_initial="${last_name:0:1}"
     url="https://www.basketball-reference.com/players/${second_initial,}/"
     search_pattern="tr.*$full_name"
+    return_string="%s: https://basketball-reference.com%s\n"
   else
     echo "Usage: -p firstname lastname"
     exit 0
   fi
 elif [ $# != 0 ] && [ $1 == "-t" ]
 then
-  if [ $# == 2 ]
+  if [ $# == 3 ]
   then
     team_name=$2
+    year=$3
     url="https://www.basketball-reference.com/teams/"
     search_pattern="full_table.*$team_name"
+    return_string="%s: https://basketball-reference.com%s$year.html\n"
   else
-    echo "Usage: -t team"
+    echo "Usage: -t team year"
     exit 0
   fi
 else
   echo "Invalid input"
   exit 0
 fi
-curl -s $url | awk -v pattern="$search_pattern" -F'\"' '$0~pattern{
+curl -s $url | awk -v pattern="$search_pattern" -v ret="$return_string" -F'\"' '$0~pattern{
   gsub ("*", "");
   gsub ("</strong>","");
   gsub ("</a></th><td class=","");
   gsub (">","");
-  printf "%s: https://basketball-reference.com%s\n",$11,$10
+  printf ret,$11,$10
 }'
