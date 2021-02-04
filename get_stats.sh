48cc87561bf2d6cceb9774d8f062ef5e4237b4ce
Kilbane <kilbanen@tcd.ie>
2021-02-04 22:24:54 +0000

    get_stats.sh only shows regular season per game stats

--- get_stats.sh
+++ get_stats.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 player_url=$1
-curl -s $player_url | awk '/per_game/ { gsub(/<[^>]*>/," "); gsub(/ +/," "); print $0 }'
+curl -s $player_url | awk '/id="per_game\.[0-9]+".*data-stat="mp_per_g"/ { gsub(/<[^>]*>/," "); gsub(/ +/,"|"); print $0 }'
