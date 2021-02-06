3806965c42a36ce55329f7ac39d2135238698122
Kilbane <kilbanen@tcd.ie>
2021-02-06 10:56:22 +0000

    Added heading

--- get_stats.sh
+++ get_stats.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 player_url=$1
-curl -s $player_url | awk '/id="per_game\.[0-9]+".*data-stat="mp_per_g"/ { gsub(/<[^>]*>/," "); gsub(/ +/,"|"); print $0 }'
+curl -s $player_url | awk '/id="per_game\.[0-9]+".*data-stat="mp_per_g"/ { gsub(/<[^>]*>/," "); gsub(/ +/,"|"); print "Season|Age|Tm|Lg|Pos|G|GS|MP|FG|FGA|FG%|3P|3PA|2P|2P%|eFG%|FT|FTA|FT%|ORB|DRB|TRB|AST|STL|BLK|TOV|PF|PTS"; print $0 }'
